3ea7003508c4c317fcefa39bd7047badf6eab76b
Niklas Pudschun <pudschun.j.n@gmx.de>
2022-01-17 17:40:41 +0100

    Adding BaseHTML Structure and base Javascript Functions

---
+++ index.js
@@ -0,0 +1,38 @@
+function addTask(){
+// create new Element
+// create new list Element
+// check if input empty
+// append all necessary parts and buttons to list item
+// append list item to Incomplete List
+
+}
+
+function editTask(){
+// get current Task
+// check if edit Flag is active or not
+// if yes change value of label  to  value of input
+// check if empty
+// or other way around
+// maybe change edit to save when in edit mode
+}
+
+function deleteTask(){
+// get Task
+// delete Task
+}
+function completeTask(){
+// move task from list incomplete to list complete
+//maybe remove edit button
+// change Done button so we can switch back to incomplete
+
+}
+function completeToIncomplete(){
+    // move Task from complete to incomplete
+    // recreate edit button
+    //rechange Done button
+}
+function bindEventsToTasks(){
+//  use hole list and iterateover it to bind all onclickfunctions
+// to their buttons and stuff so we dont have to do it in all the
+// other functions
+}
